c50d75fbd935f8eab785bf3bf5de1116e4daeb48
gaddamranadheerreddy <gaddamranadheerreddy44@gmail.com>
2025-06-03 15:54:37 +0530

    Modifies the code to look more intuitive.  -> Client -> Student -> getBuilder -> Builder object(created with given attributes) -> build() - meaning creating student object.

--- src/main/groovy/com/scalar/lldmodule/DesignPatterns/Builder/Client.java
+++ src/main/groovy/com/scalar/lldmodule/DesignPatterns/Builder/Client.java
@@ -3,14 +3,24 @@ package com.scalar.lldmodule.DesignPatterns.Builder;
 public class Client {
     public static void main(String[] args) {
 //        Builder builder = new Builder();
-        Builder builder = Student.getBuilder();
-        builder.setName("Rana");
-        builder.setAge(23);
-        builder.setPsp(88);
-        builder.setGradYear(2022);
+//        Builder builder = Student.getBuilder();
+//        builder.setName("Rana");
+//        builder.setAge(23);
+//        builder.setPsp(88);
+//        builder.setGradYear(2022);
+//
+////        Student student = new Student(builder);
+//        Student student = builder.build();
 
-//        Student student = new Student(builder);
-        Student student = builder.build();
+        Student student =
+                Student.getBuilder()
+                                .setName("Rana")
+                                .setAge(23)
+                                .setPsp(88)
+                                .setGradYear(2022)
+                                .setBatch("new batch")
+                .build();
+
         System.out.println("DEBUG: ");
     }
 }
--- src/main/groovy/com/scalar/lldmodule/DesignPatterns/Builder/Student.java
+++ src/main/groovy/com/scalar/lldmodule/DesignPatterns/Builder/Student.java
@@ -10,12 +10,16 @@ public class Student {
     private int gradYear;
     private String phoneNumber;
 
+    //This is static method
+    // -> If calling non-static  -> gives error
+    // -> So should call only static methods/class
+    // -> So make Builder class static.
     public static Builder getBuilder(){
         return new Builder();
     }
 
     //Student Constructor -> Public -> AnyOnce can create Student Object.
-    public Student(Builder builder) {
+    private Student(Builder builder) {
         //Validations -> Start
         if(builder.getGradYear() > 2024){
             throw new RuntimeException("grad Year shouldn't be grater than current year");
@@ -31,5 +35,98 @@ public class Student {
         this.gradYear = builder.getGradYear();
         this.phoneNumber = builder.getPhoneNumber();
 
+    }
+
+    //Inner Class
+    static class Builder {
+        private String name;
+        private int age;
+        private double psp;
+        private String universityName;
+        private String batch;
+        private long id;
+        private int gradYear;
+        private String phoneNumber;
+
+        public Student build() {
+            return new Student(this); //Private Student Constructor
+            // -> cannot be accessed from Outside of the class.
+            //Lets move this class inside the Student class
+            // -> So that we can access the private Constructor of Student class.
+            //Now the private Student constructor can be accessed
+            // -> as we are inside the Student Class now.
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public String getUniversityName() {
+            return universityName;
+        }
+
+        public Builder setUniversityName(String universityName) {
+            this.universityName = universityName;
+            return this;
+        }
+
+        public String getBatch() {
+            return batch;
+        }
+
+        public Builder setBatch(String batch) {
+            this.batch = batch;
+            return this;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public Builder setId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public int getGradYear() {
+            return gradYear;
+        }
+
+        public Builder setGradYear(int gradYear) {
+            this.gradYear = gradYear;
+            return this;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
     }
 }
